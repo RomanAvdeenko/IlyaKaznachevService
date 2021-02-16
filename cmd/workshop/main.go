205aeba2ea5d81e05bf8ff30745ceccf448bc62b
RomanAvdeenko <yes@hl.ua>
2021-02-16 15:53:34 +0200

    Add config parsing

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -4,16 +4,24 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/RomanAvdeenko/IlyaKaznachevService/internal/config"
 	"github.com/RomanAvdeenko/IlyaKaznachevService/internal/handler"
 	"github.com/go-chi/chi"
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 func main() {
+	cfg := config.Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	h := handler.NewHandler()
 	r := chi.NewRouter()
 
 	r.Get("/hello", h.Hello)
-	err := http.ListenAndServe(":8080", r)
+	err = http.ListenAndServe(cfg.Host+":"+cfg.Port, r)
 	if err != nil {
 		log.Fatalln("Can't start server")
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/RomanAvdeenko/IlyaKaznachevService
 
 go 1.15
 
-require github.com/go-chi/chi v1.5.2 // indirect
+require (
+	github.com/go-chi/chi v1.5.2 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.2.5 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
